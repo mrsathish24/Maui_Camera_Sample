6fdf5f77efa741081b2f40e722e63e8ed1b7c19e
Sathishkumar M <sathishkumar.muthusamy@acsys.com>
2025-03-18 12:08:38 +0530

    first commit

---
+++ MauiCameraMauiSample/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace MauiCameraMauiSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void cameraView_CamerasLoaded(object sender, EventArgs e)
+        {
+            cameraView.Camera = cameraView.Cameras.First();
+
+            MainThread.BeginInvokeOnMainThread(async () =>
+            {
+                await cameraView.StopCameraAsync();
+                await cameraView.StartCameraAsync();
+            });
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            myImage.Source = cameraView.GetSnapShot(Camera.MAUI.ImageFormat.PNG);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# .NET MAUI Camera.MAUI Sample
+
+Sample code to demonstrate how to work with the in .NET MAUI
+# Maui_Camera_Sample
